44aeb5b6569fc7488d007f6a5628bc2bf6c1790d
joseph shahin <josephchahine@live.com>
2022-08-29 00:39:40 +0200

    added products

--- shoppy/product/models.py
+++ shoppy/product/models.py
@@ -1,3 +1,6 @@
+from codecs import oem_decode
+from msilib.schema import Class
+from unicodedata import category
 from django.db import models
 
 class Category(models.Model):
@@ -9,3 +12,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+Class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
