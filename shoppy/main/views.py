67e0069b138ef3c9d998565e6a84547b8810cf80
11zouzou11 <josephchahine@live.com>
2022-08-29 00:32:29 +0000

    added the search filter to the products

--- shoppy/main/views.py
+++ shoppy/main/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 def mainpage(request):
     products = Product.objects.all()[0:8]
@@ -8,6 +9,23 @@ def mainpage(request):
     return render(request, 'main/mainpage.html', {'products': products})
 
 def shop(request):
-    products = Product.objects.all()[0:8]
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    active_category = request.GET.get('category','')
+    
+    if active_category:
+        products = products.filter(category__slug=active_category)
 
-    return render(request, 'main/shop.html', {'products': products})
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'categories' : categories,
+        'products': products,
+        'active_category': active_category
+    }
+
+
+    return render(request, 'main/shop.html', context)
